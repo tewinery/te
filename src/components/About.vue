a8cac576414f473665b2eafb679d4bfc140719ee
Fred Rivett <fred@fredrivett.com>
2018-05-23 15:50:11 -0700

    Style About section

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,38 +2,50 @@
   <div>
     <section class="section o_block o_block--large has-background-grey-light has-text-centered">
       <div class="container">
-        <div class="o_block o_block--bottom">
-          <h2 class="subtitle">About</h2>
+        <div class="columns is-centered">
+          <div class="column is-two-thirds-desktop">
+            <div class="o_block o_block--bottom">
+              <h2 class="subtitle">About</h2>
 
-          <p class="is-size-3">
-            Té was founded by Ross Kenyon who’s roots go back to the industrial
-            heartlands of Yorkshire, where tea is more than a drink, it’s a way of
-            life.
-          </p>
-          <p>
-            Ross shares his enthusiasm for tea with a passion for fine wine and a
-            wonderment that through leaf or grape the culture, history and climate
-            of a place is revealed.
-          </p>
-          <p>
-            This has led him to ask the question, “Can I capture the essence of a
-            place through its tea, within a fine wine?”
-          </p>
-          <p>
-            And many months, many trials and several years later the answer
-            to that question is, “Yes!” And with it Té Wine was born.
-          </p>
+              <p class="is-size-3">
+                Té was founded by Ross Kenyon who’s roots go back to the industrial
+                heartlands of Yorkshire, where tea is more than a drink, it’s a way of
+                life.
+              </p>
+            </div>
+          </div>
         </div>
 
         <div class="columns is-centered">
           <div class="column is-two-thirds-tablet is-half-desktop">
-            <img
-              src="~@/assets/images/headshot.png"
-              alt="Photo of Ross Kenyon, founder of Té"
-            />
+            <div class="o_block o-block--bottom">
+              <img
+                src="~@/assets/images/headshot.png"
+                alt="Photo of Ross Kenyon, founder of Té"
+              />
+            </div>
           </div>
         </div>
 
+        <div class="columns is-centered">
+          <div class="column is-half-desktop">
+            <p>
+              Ross shares his enthusiasm for tea with a passion for fine wine and a
+              wonderment that through leaf or grape the culture, history and climate
+              of a place is revealed.
+            </p>
+            <p class="is-size-3">
+              This has led him to ask the question,
+              <br class="is-hidden-mobile is-hidden-tablet-only is-hidden-desktop-only" />
+              “Can I capture the essence of a place through its tea, within a
+              fine&nbsp;wine?”
+            </p>
+            <p>
+              And many months, many trials and several years later the answer
+              to that question is, “Yes!” And with it Té Wine was born.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
     <section class="section o_block has-background-dark has-text-light has-text-centered">
