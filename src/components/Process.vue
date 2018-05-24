063b008261f9494dffbaa9b98a46136ac17d620b
Fred Rivett <fred@fredrivett.com>
2018-05-24 10:17:55 -0700

    Style Process section

--- src/components/Process.vue
+++ src/components/Process.vue
@@ -1,33 +1,47 @@
 <template>
-  <section class="section o_block has-background-grey-dark has-text-centered has-text-light">
+  <section class="section o_block o_block--large o_block--mb-default has-background-grey-dark has-text-centered has-text-light">
     <div class="container">
       <h2 class="subtitle">Process</h2>
       <p class="is-size-3">The four steps we take to make our wine:</p>
 
-      <div class="columns o_block o_block--large">
-        <div class="column">
-          <h3 class="subtitle">Step one</h3>
-          <p>We brew the finest loose leaf tea with filtered water</p>
+      <div class="columns o_block is-multiline">
+        <div class="column is-half-tablet is-one-quarter-desktop is-flex">
+          <div class="c_card has-background-medium-light-blue">
+            <h3 class="subtitle">Step one</h3>
+            <p class="c_card__main">We brew the finest loose leaf tea with filtered water</p>
+          </div>
         </div>
-        <div class="column">
-          <h3 class="subtitle">Step two</h3>
-          <p>The tea is then sweetened and yeast is added</p>
+        <div class="column is-half-tablet is-one-quarter-desktop is-flex">
+          <div class="c_card has-background-white-bis">
+            <h3 class="subtitle">Step two</h3>
+            <p class="c_card__main">The tea is then sweetened and yeast is added</p>
+          </div>
         </div>
-        <div class="column">
-          <h3 class="subtitle">Step three</h3>
-          <p>We ferment the tea under temperature control</p>
+        <div class="column is-half-tablet is-one-quarter-desktop is-flex">
+          <div class="c_card has-background-light">
+            <h3 class="subtitle">Step three</h3>
+            <p class="c_card__main">We ferment the tea under temperature control</p>
+          </div>
         </div>
-        <div class="column">
-          <h3 class="subtitle">Step four</h3>
-          <p>We adjust the sweetness to compliment the complex flavours</p>
+        <div class="column is-half-tablet is-one-quarter-desktop is-flex">
+          <div class="c_card has-background-white-bis">
+            <h3 class="subtitle">Step four</h3>
+            <p class="c_card__main">We adjust the sweetness to compliment the complex flavours</p>
+          </div>
         </div>
       </div>
 
-      <p>
-        Té is a luxury wine because we only use the finest loose leaf teas and
-        the price per tonne of tea is much higher the price per tonne of grape
-        and it is for that reason that our wines are reassuringly expensive.
-      </p>
+      <div class="columns is-centered has-text-dark has-horizontal-lines">
+        <div class="column is-two-thirds-tablet is-half-desktop o_bookend">
+          <span class="o_bookend__left"></span>
+          <p class="is-marginless">
+            Té is a luxury wine because we only use the finest loose leaf teas and
+            the price per tonne of tea is much higher the price per tonne of grape
+            and it is for that reason that our wines are reassuringly expensive.
+          </p>
+          <span class="o_bookend__right"></span>
+        </div>
+      </div>
     </div>
   </section>
 </template>
