488a0075704e5776efb57e7ba4388dace5be60c5
Fred Rivett <fred@fredrivett.com>
2018-07-22 19:52:54 +0100

    Replace Quality section with Give

--- src/components/Give.vue
+++ src/components/Give.vue
@@ -1,18 +1,11 @@
 <template>
   <section class="section has-background-light has-background-dark has-text-light has-text-centered">
     <div class="section o_block o_block--large c_giving-back" v-in-viewport>
-      <h3 class="subtitle is-marginless c_giving-back__header">Quality</h3>
+      <h3 class="subtitle is-marginless c_giving-back__header">Give</h3>
       <div class="columns is-marginless is-centered">
         <div class="column is-two-thirds-desktop is-half-desktop">
           <p class="is-size-3">
-            Our wines are made in small batches using state-of-the-art
-            equipment, which regulates the fermentation and minimises dissolved
-            oxygen pickup
-          </p>
-
-          <p class="is-size-3">
-            That and using the world's finest teas make our wines reassuringly
-            expensive
+            Té Wine give ten percent of their profits to support eye care in the tea producing nations
           </p>
         </div>
       </div>
