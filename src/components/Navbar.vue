1cfc828a32d9ad9927e36f833fa92dc02e16cdda
Fred Rivett <fred@fredrivett.com>
2019-10-09 19:14:52 +0100

    Remove links from navbar, replace with space for UI hack

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,7 @@
 <template>
   <nav class="c_navbar has-background-primary">
     <div class="c_navbar__bar">
-      <ul class="c_navbar__social">
-        <li class="c_navbar__social-item">
-          <a href="https://twitter.com/tewinery" target="_blank">Twitter</a>
-        </li>
-        <li class="c_navbar__social-item">
-          <a href="https://www.instagram.com/tewinery/" target="_blank">Instagram</a>
-        </li>
-      </ul>
+      &nbsp;
     </div>
   </nav>
 </template>
